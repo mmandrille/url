6f0d86a87c8d38d77eff70f7dd9b4f5a0652b5f2
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-06-05 17:48:55 -0300

    Modificaciones menores

--- url/settings.py
+++ url/settings.py
@@ -113,8 +113,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/static/'
-
 #Configuraciones Propias
 MEDIA_URL = 'archivos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "archivos")
@@ -129,12 +127,17 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = '/'
 
-# Configurar correos
+#Configuracion de Mail
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = ''
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tusuario@gmail.com'
+EMAIL_HOST_USER = 'tucorreo@dominio.com'
 EMAIL_HOST_PASSWORD = 'tupassword'
+#CREAR ARCHIVO credenciales.py solo para Sobre Escribir con las variables importadas
+try:
+    from .credenciales import *
+except ImportError:
+    pass
 
 
 #Actualizar Statics, no solo nuevas
